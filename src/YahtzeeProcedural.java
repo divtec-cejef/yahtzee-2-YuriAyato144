efecd2de8d40860823eed598680dfc2a0910136b
YuriAyato144 <aedan.belet@divtec.ch>
2025-08-20 10:40:51 +0200

    Jalon 1 (correction 1)

--- src/YahtzeeProcedural.java
+++ src/YahtzeeProcedural.java
@@ -1,7 +1,36 @@
+import java.util.Arrays;
+
 public class YahtzeeProcedural {
+    /**
+     * Lance un dé qui retourne un nombre aléatoire.
+     * @return un nombre aléatoire de 1 à nombre de face défini.
+     */
+    public static int De (){
+         int nombreFace = 6;
+
+        return (int) (Math.random() * nombreFace) + 1;
+    }
+
+    /**
+     * Liste contenant le nombre de dé au nombre aléatoire.
+     * @return les différent
+     */
+    public static int[] tableau() {
+       int[] nombreDe =  new int[5];
+       for (int lancer = 0; lancer < nombreDe.length; lancer++) {
+           nombreDe[lancer] = De();
+       }
+        return nombreDe;
+    }
+
+    public static void afficherDe(int[] listeDe){
+        for (int afficher = 0; afficher < tableau().length; afficher++) {
+            System.out.print("Jet " + (afficher + 1) + " : " +  listeDe[afficher] + "\n");
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println( "Bon courage !");
-
+        afficherDe(tableau());
     }
+
 }
